047a8977ae7f284b4f35f300706c347e19ad2115
Martin Pengelly-Phillips <martin@ftrack.com>
2014-12-11 17:12:00 +0000

    Configure sensible defaults for Sequence and Episode.
    This is useful for now, but ultimately should be handled in the backend.

--- resource/plugin/construct_entity_type.py
+++ resource/plugin/construct_entity_type.py
@@ -4,9 +4,34 @@
 import ftrack.entity
 
 
+def default_task_status(entity):
+    '''Return default task status entity for *entity*.'''
+    return entity.session.query('TaskStatus')[0]
+
+
+def default_task_type(entity):
+    '''Return default task type entity for *entity*.'''
+    return entity.session.query('TaskType')[0]
+
+
+def default_task_priority(entity):
+    '''Return default task priority entity for *entity*.'''
+    return entity.session.query('PriorityType')[0]
+
+
 def construct_entity_type(event):
     '''Return class to represent entity type specified by *event*.'''
-    return ftrack.entity.class_factory(event['data']['schema'])
+    schema = event['data']['schema']
+    cls = ftrack.entity.class_factory(schema)
+
+    if schema['id'] in ('Episode', 'Sequence'):
+        # Add dynamic default values to appropriate attributes so that end users
+        # don't need to specify them each time.
+        cls.attributes.get('status').default_value = default_task_status
+        cls.attributes.get('type').default_value = default_task_type
+        cls.attributes.get('priority').default_value = default_task_priority
+
+    return cls
 
 
 def register(session):
