d9013e44a4776f9687afe7fc01c023d2a0504000
Martin Pengelly-Phillips <martin@ftrack.com>
2014-12-17 13:04:17 +0000

    Include inherited members in autodoc descriptions.

--- doc/conf.py
+++ doc/conf.py
@@ -68,7 +68,7 @@ html_copy_source = True
 
 # -- Autodoc ------------------------------------------------------------------
 
-autodoc_default_flags = ['members', 'undoc-members']
+autodoc_default_flags = ['members', 'undoc-members', 'inherited-members']
 autodoc_member_order = 'bysource'
 
 
