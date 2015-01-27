cfce37ff97a5392a855578f12419055e43b1e6a2
Björn Rydahl <bjorn.rydahl@gmail.com>
2015-01-27 14:13:41 +0100

    Added create project test and updated project schema with helper methods to get types and statuses.

--- resource/plugin/construct_entity_type.py
+++ resource/plugin/construct_entity_type.py
@@ -19,19 +19,83 @@ def default_task_priority(entity):
     return entity.session.query('PriorityType')[0]
 
 
+class ProjectSchema(ftrack.entity.base.Entity):
+    '''Class representing ProjectSchema.'''
+
+    def get_statuses(self, schema, type_id=None):
+        '''Return statuses for *schema* and optional *type_id*.
+
+        *type_id* is the id of the TaskType for a Task and can be used to get
+        statuses where the workflow has been overridden.
+
+        '''
+        # TODO: Refactor this once arbitrary context is supported on server.
+        if schema == 'Task':
+            if type_id is not None:
+                overrides = self['_overrides']
+                for override in overrides:
+                    if override['type_id'] == type_id:
+                        return override['workflow_scheme']['statuses'][:]
+
+            return self['_task_workflow']['statuses'][:]
+
+        elif schema == 'Shot':
+            return self['_shot_workflow']['statuses'][:]
+
+        elif schema == 'AssetVersion':
+            return self['_version_workflow']['statuses'][:]
+
+        elif schema == 'AssetBuild':
+            for _schema in self['_schemas']:
+                if _schema['type_id'] == '4be63b64-5010-42fb-bf1f-428af9d638f0':
+                    result = self.session.query(
+                        'select task_status from SchemaStatus'
+                        ' where schema_id is {0}'.format(_schema['id'])
+                    )
+                    return [
+                        schema_type['task_status'] for schema_type in result
+                    ]
+
+        raise ValueError('Schema {0} does not have statuses.'.format(schema))
+
+    def get_types(self, schema):
+        '''Return statuses for *schema*.'''
+        # TODO: Refactor this once arbitrary context is supported on server.
+        if schema == 'Task':
+            return self['_task_type_scheme']['types'][:]
+
+        elif schema == 'AssetBuild':
+            for _schema in self['_schemas']:
+                if _schema['type_id'] == '4be63b64-5010-42fb-bf1f-428af9d638f0':
+                    result = self.session.query(
+                        'select task_type from SchemaType'
+                        ' where schema_id is {0}'.format(_schema['id'])
+                    )
+                    return [schema_type['task_type'] for schema_type in result]
+
+        raise ValueError('Schema {0} does not have types.'.format(schema))
+
+
 class Factory(ftrack.entity.factory.Factory):
     '''Entity class factory.'''
 
     def create(self, schema, bases=None):
         '''Create and return entity class from *schema*.'''
-        cls = super(Factory, self).create(schema, bases=bases)
+        if schema['id'] == 'ProjectSchema':
+            cls = super(Factory, self).create(schema, bases=[ProjectSchema])
+        else:
+            cls = super(Factory, self).create(schema, bases=bases)
 
+        # Add dynamic default values to appropriate attributes so that end
+        # users don't need to specify them each time.
         if schema['id'] in ('Episode', 'Sequence'):
-            # Add dynamic default values to appropriate attributes so that end
-            # users don't need to specify them each time.
             cls.attributes.get('status').default_value = default_task_status
-            cls.attributes.get('type').default_value = default_task_type
+
+        if schema['id'] in ('Episode', 'Sequence', 'Shot', 'AssetBuild', 'Task'):
             cls.attributes.get('priority').default_value = default_task_priority
+
+        if schema['id'] in ('Episode', 'Sequence', 'Shot'):
+            cls.attributes.get('type').default_value = default_task_type
 
         return cls
 
