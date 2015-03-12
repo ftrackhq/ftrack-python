9658cfaa243df07d00664fef8b8c506746a620c9
Martin Pengelly-Phillips <martin@ftrack.com>
2015-03-12 15:01:49 +0000

    Store hard coded location ids in symbol module.

--- source/ftrack/symbol.py
+++ source/ftrack/symbol.py
@@ -29,3 +29,15 @@ class Symbol(object):
 
 #: Symbol representing that no value has been set or loaded.
 NOT_SET = Symbol('NOT_SET', False)
+
+#: Identifier of builtin origin location.
+ORIGIN_LOCATION_ID = 'ce9b348f-8809-11e3-821c-20c9d081909b'
+
+#: Identifier of builtin unmanaged location.
+UNMANAGED_LOCATION_ID = 'cb268ecc-8809-11e3-a7e2-20c9d081909b'
+
+#: Identifier of builtin review location.
+REVIEW_LOCATION_ID = 'cd41be70-8809-11e3-b98a-20c9d081909b'
+
+#: Identifier of builtin connect location.
+CONNECT_LOCATION_ID = '07b82a97-8cf9-11e3-9383-20c9d081909b'
