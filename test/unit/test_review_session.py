3e6776ea9e7fd3a43f59aebb3eb85c0234eaca3d
Carl Claesson <carl.claesson@ftrack.com>
2015-04-23 11:05:01 +0200

    Added append and pop test for review session object and invitee.

--- test/unit/test_review_session.py
+++ test/unit/test_review_session.py
@@ -87,6 +87,56 @@ def test_add_remove_review_session_objects(
     assert len(review_session['review_session_objects']) == 0
 
 
+def test_add_object_with_append(session, new_review_session, unique_name):
+    '''Test adding object using append.'''
+    new_review_session_id = new_review_session['id']
+
+    # Get a reviewable AssetVersion from the 'client review' project.
+    asset_version = session.get(
+        'AssetVersion', 'a7519019-5910-11e4-804a-3c0754282242'
+    )
+
+    review_session_object_id = str(uuid.uuid1())
+    review_session_object = session.create('ReviewSessionObject', {
+        'id': review_session_object_id,
+        'name': unique_name,
+        'description': unique_name,
+        'version': 'Version {0}'.format(asset_version['version']),
+        'asset_version': asset_version
+    })
+
+    new_review_session['review_session_objects'].append(
+        review_session_object
+    )
+
+    session.commit()
+
+    session = ftrack.Session()
+
+    review_session = session.get('ReviewSession', new_review_session_id)
+    review_session_objects = review_session['review_session_objects']
+
+    assert len(review_session_objects) == 1
+
+
+def test_remove_object_with_pop(
+    session, new_review_session_object, unique_name
+):
+    '''Test adding object using append.'''
+    review_session = new_review_session_object['review_session']
+    review_session_id = review_session['id']
+
+    review_session['review_session_objects'].pop()
+    session.commit()
+
+    session = ftrack.Session()
+
+    review_session = session.get('ReviewSession', review_session_id)
+    review_session_objects = review_session['review_session_objects']
+
+    assert len(review_session_objects) == 0
+
+
 def test_add_remove_review_session_invitee(session, new_review_session):
     '''Test add and remove invitees from review session.'''
     review_session_invitees = new_review_session['review_session_invitees']
@@ -119,6 +169,53 @@ def test_add_remove_review_session_invitee(session, new_review_session):
     review_session_invitee = session.get('ReviewSessionInvitee', invitee_id)
 
     assert not review_session_invitee
+
+
+def test_add_invitee_with_append(session, new_review_session, unique_name):
+    '''Test adding object using append.'''
+    new_review_session_id = new_review_session['id']
+
+    review_session_invitee = session.create('ReviewSessionInvitee', {
+        'email': 'john.doe@example.com',
+        'name': 'John Doe',
+        'review_session': new_review_session
+    })
+
+    new_review_session['review_session_invitees'].append(
+        review_session_invitee
+    )
+
+    session.commit()
+
+    session = ftrack.Session()
+
+    review_session = session.get('ReviewSession', new_review_session_id)
+    review_session_objects = review_session['review_session_invitees']
+
+    assert len(review_session_objects) == 1
+
+
+def test_remove_invitee_with_pop(session, new_review_session, unique_name):
+    '''Test adding object using append.'''
+    review_session_id = new_review_session['id']
+
+    session.create('ReviewSessionInvitee', {
+        'email': 'john.doe@example.com',
+        'name': 'John Doe',
+        'review_session': new_review_session
+    })
+
+    session.commit()
+
+    new_review_session['review_session_invitees'].pop()
+    session.commit()
+
+    session = ftrack.Session()
+
+    review_session = session.get('ReviewSession', review_session_id)
+    review_session_objects = review_session['review_session_invitees']
+
+    assert len(review_session_objects) == 0
 
 
 def test_add_remove_review_session_object_statuses(
