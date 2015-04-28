ddb0b9112e61cce7cbc0739e4f39291fe7960676
Martin Pengelly-Phillips <martin@ftrack.com>
2015-04-28 12:34:33 +0100

    Move tests into correct modules.

--- test/unit/test_caching.py
+++ test/unit/test_caching.py
@@ -1,8 +1,6 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2015 ftrack
 
-import pytest
-
 import ftrack.inspection
 
 
@@ -34,15 +32,3 @@ def test_get_entity_from_cache(session, user, mocker):
 
     # Check that no call was made to server.
     assert not session._call.called
-
-
-def test_get_non_existant_entity(session):
-    '''Retrieve a non-existant entity by type and id.'''
-    matching = session.get('User', 'non-existant-id')
-    assert matching is None
-
-
-def test_get_entity_of_invalid_type(session):
-    '''Fail to retrieve an entity using an invalid type.'''
-    with pytest.raises(KeyError):
-        session.get('InvalidType', 'id')
--- test/unit/test_session.py
+++ test/unit/test_session.py
@@ -4,3 +4,21 @@
 import pytest
 
 import ftrack.inspection
+
+
+def test_get_entity(session, user):
+    '''Retrieve an entity by type and id.'''
+    matching = session.get(*ftrack.inspection.identity(user))
+    assert matching == user
+
+
+def test_get_non_existant_entity(session):
+    '''Retrieve a non-existant entity by type and id.'''
+    matching = session.get('User', 'non-existant-id')
+    assert matching is None
+
+
+def test_get_entity_of_invalid_type(session):
+    '''Fail to retrieve an entity using an invalid type.'''
+    with pytest.raises(KeyError):
+        session.get('InvalidType', 'id')
