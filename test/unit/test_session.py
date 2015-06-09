faf7a190bce280783dadda5ec291f6e7678645d7
Martin Pengelly-Phillips <martin@ftrack.com>
2015-06-09 11:39:22 +0100

    Add basic operation ordering unit test.

--- test/unit/test_session.py
+++ test/unit/test_session.py
@@ -22,3 +22,28 @@ def test_get_entity_of_invalid_type(session):
     '''Fail to retrieve an entity using an invalid type.'''
     with pytest.raises(KeyError):
         session.get('InvalidType', 'id')
+
+
+def test_operation_ordering(session, unique_name):
+    '''Perform operations in predictable order on commit.'''
+    # Delete ordering.
+    project_schema = session.query('ProjectSchema')[0]
+    project = session.create('Project', {
+        'name': unique_name,
+        'full_name': unique_name,
+        'project_schema': project_schema
+    })
+
+    sequence = session.create('Sequence', {
+        'name': unique_name,
+        'parent': project
+    })
+
+    session.commit()
+
+    session.delete(sequence)
+    session.delete(project)
+
+    # Should not fail.
+    session.commit()
+
