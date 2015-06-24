cb74d4404015e9332b83eb868c592bc566ef5d71
Martin Pengelly-Phillips <martin@ftrack.com>
2015-06-24 17:55:21 +0100

    Fix bug on Windows where file must seek to current position when switching from read to write.

--- source/ftrack_api/data.py
+++ source/ftrack_api/data.py
@@ -56,10 +56,13 @@ class FileWrapper(Data):
     def __init__(self, wrapped_file):
         '''Initialise access to *wrapped_file*.'''
         self.wrapped_file = wrapped_file
+        self._read_since_last_write = False
         super(FileWrapper, self).__init__()
 
     def read(self, limit=None):
         '''Return content from current position up to *limit*.'''
+        self._read_since_last_write = True
+
         if limit is None:
             limit = -1
 
@@ -67,7 +70,12 @@ class FileWrapper(Data):
 
     def write(self, content):
         '''Write content at current position.'''
+        if self._read_since_last_write:
+            # Windows requires a seek before switching from read to write.
+            self.seek(self.tell())
+
         self.wrapped_file.write(content)
+        self._read_since_last_write = False
 
     def flush(self):
         '''Flush buffers ensuring data written.'''
