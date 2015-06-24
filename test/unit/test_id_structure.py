da9628baa2c6218c4f9323199c77e6c7381ddd90
Martin Pengelly-Phillips <martin@ftrack.com>
2015-06-24 15:31:59 +0100

    Make structure test follow standard pytest style.

--- test/unit/test_id_structure.py
+++ test/unit/test_id_structure.py
@@ -5,45 +5,35 @@ import ftrack_api
 import ftrack_api.structure.id
 
 
-class TestIdStructure(object):
-    '''Test id structure.'''
+def test_get_resource_identifier(session):
+    '''Get resource identifier from structure.'''
+    file_component = session.create('FileComponent', {
+        'name': 'my_component_name',
+        'file_type': '.png'
+    })
 
-    def setup_method(self, method):
-        '''Setup the test.'''
+    structure = ftrack_api.structure.id.IdStructure(prefix='/path')
 
-    def teardown_method(self, method):
-        '''Teardown the test.'''
+    resource_identifier = structure.get_resource_identifier(file_component)
 
-    def test_get_resource_identifier(self):
-        '''Get resource identifier from structure.'''
-        session = ftrack_api.Session()
-        file_component = session.create('FileComponent', {
-            'name': 'my_component_name',
-            'file_type': '.png'
-        })
+    assert resource_identifier.startswith('path/')
+    assert resource_identifier.endswith(file_component['id'][4:] + '.png')
 
-        structure = ftrack_api.structure.id.IdStructure(prefix='/path')
+    sequence_component = session.create('SequenceComponent', {
+        'name': 'my sequence component'
+    })
 
-        resource_identifier = structure.get_resource_identifier(file_component)
+    file_component = session.create('FileComponent', {
+        'name': 'file_component_name',
+        'container': sequence_component,
+        'file_type': '.jpg'
+    })
 
-        assert resource_identifier.startswith('path/')
-        assert resource_identifier.endswith(file_component['id'][4:] + '.png')
+    resource_identifier = structure.get_resource_identifier(
+        file_component
+    )
 
-        sequence_component = session.create('SequenceComponent', {
-            'name': 'my sequence component'
-        })
-
-        file_component = session.create('FileComponent', {
-            'name': 'file_component_name',
-            'container': sequence_component,
-            'file_type': '.jpg'
-        })
-
-        resource_identifier = structure.get_resource_identifier(
-            file_component
-        )
-
-        assert resource_identifier.startswith('path/')
-        assert resource_identifier.endswith(
-            sequence_component['id'][4:] + '/file.file_component_name.jpg'
-        )
+    assert resource_identifier.startswith('path/')
+    assert resource_identifier.endswith(
+        sequence_component['id'][4:] + '/file.file_component_name.jpg'
+    )
