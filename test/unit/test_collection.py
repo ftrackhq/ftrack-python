dc830c4f5a6194e09b3af6852f58d235cac073e8
Martin Pengelly-Phillips <martin@ftrack.com>
2015-07-01 14:30:04 +0100

    Add failing unit test to highlight shallow copy of collection issue.
    Currently this issues causes a Create operation to record more data than it should leading to server side errors when referenced
    entities do not exist yet.

--- test/unit/test_collection.py
+++ test/unit/test_collection.py
@@ -1,6 +1,8 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2015 ftrack
 
+import copy
+
 import ftrack_api.collection
 import ftrack_api.symbol
 import ftrack_api.inspection
@@ -13,3 +15,17 @@ def test_initialisation_does_not_modify_entity_state(new_user):
     )
 
     assert ftrack_api.inspection.state(new_user) is ftrack_api.symbol.NOT_SET
+
+
+def test_shallow_copy(new_user):
+    '''Shallow copying collection should avoid indirect mutation.'''
+    collection = ftrack_api.collection.Collection(
+        new_user, None, data=[1, 2]
+    )
+
+    with new_user.session.operation_recording(False):
+        collection_copy = copy.copy(collection)
+        collection_copy.append(3)
+
+    assert set(collection) == set([1, 2])
+    assert set(collection_copy) == set([1, 2, 3])
