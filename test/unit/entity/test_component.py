0426d5a632201267812c82c4e72a9b4e134e3443
Martin Pengelly-Phillips <martin@ftrack.com>
2015-07-23 11:01:40 +0100

    Add tests for mixin helper.

---
+++ test/unit/entity/test_component.py
@@ -0,0 +1,10 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2015 ftrack
+
+import ftrack_api
+import ftrack_api.inspection
+
+
+
+def test_get_availability(locations):
+    '''Retrieve availability in locations.'''
