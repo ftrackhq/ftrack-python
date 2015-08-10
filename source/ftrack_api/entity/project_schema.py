8951fb1d8b54486e2d011607d5c7adfccd7c0a32
Björn Rydahl <bjorn.rydahl@gmail.com>
2015-08-10 14:53:44 +0200

    Updated convenience methods to work with dynamic schemas.

--- source/ftrack_api/entity/project_schema.py
+++ source/ftrack_api/entity/project_schema.py
@@ -24,15 +24,28 @@ class ProjectSchema(ftrack_api.entity.base.Entity):
 
             return self['_task_workflow']['statuses'][:]
 
-        elif schema == 'Shot':
-            return self['_shot_workflow']['statuses'][:]
-
         elif schema == 'AssetVersion':
             return self['_version_workflow']['statuses'][:]
 
-        elif schema == 'AssetBuild':
+        else:
+            try:
+                EntityTypeClass = self.session.types[schema]
+            except KeyError:
+                raise ValueError('Schema {0} does not exist.'.format(schema))
+
+            object_type_id_attribute = EntityTypeClass.attributes.get(
+                'object_type_id'
+            )
+
+            try:
+                object_type_id = object_type_id_attribute.default_value
+            except AttributeError:
+                raise ValueError(
+                    'Schema {0} does not have statuses.'.format(schema)
+                )
+
             for _schema in self['_schemas']:
-                if _schema['type_id'] == '4be63b64-5010-42fb-bf1f-428af9d638f0':
+                if _schema['type_id'] == object_type_id:
                     result = self.session.query(
                         'select task_status from SchemaStatus '
                         'where schema_id is {0}'.format(_schema['id'])
@@ -41,7 +54,9 @@ class ProjectSchema(ftrack_api.entity.base.Entity):
                         schema_type['task_status'] for schema_type in result
                     ]
 
-        raise ValueError('Schema {0} does not have statuses.'.format(schema))
+            raise ValueError(
+                'No valid statuses were found for schema {0}.'.format(schema)
+            )
 
     def get_types(self, schema):
         '''Return statuses for *schema*.'''
@@ -49,13 +64,31 @@ class ProjectSchema(ftrack_api.entity.base.Entity):
         if schema == 'Task':
             return self['_task_type_schema']['types'][:]
 
-        elif schema == 'AssetBuild':
+        else:
+            try:
+                EntityTypeClass = self.session.types[schema]
+            except KeyError:
+                raise ValueError('Schema {0} does not exist.'.format(schema))
+
+            object_type_id_attribute = EntityTypeClass.attributes.get(
+                'object_type_id'
+            )
+
+            try:
+                object_type_id = object_type_id_attribute.default_value
+            except AttributeError:
+                raise ValueError(
+                    'Schema {0} does not have types.'.format(schema)
+                )
+
             for _schema in self['_schemas']:
-                if _schema['type_id'] == '4be63b64-5010-42fb-bf1f-428af9d638f0':
+                if _schema['type_id'] == object_type_id:
                     result = self.session.query(
                         'select task_type from SchemaType '
                         'where schema_id is {0}'.format(_schema['id'])
                     )
                     return [schema_type['task_type'] for schema_type in result]
 
-        raise ValueError('Schema {0} does not have types.'.format(schema))
+            raise ValueError(
+                'No valid types were found for schema {0}.'.format(schema)
+            )
