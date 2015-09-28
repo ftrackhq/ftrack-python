6c4af37d31f440b0b92378afda2b932b32dd2d74
Mattias Lagergren <mattias.lagergren@ftrack.com>
2015-09-28 16:07:29 +0200

    Update required server version

--- test/unit/test_session.py
+++ test/unit/test_session.py
@@ -460,8 +460,8 @@ def test_populate_entity_with_composite_primary_key(session, new_project):
     ({}, False),
     ({'version': '3.2.1'}, True),
     ({'version': '3.2.2'}, True),
-    ({'version': '3.3'}, False),
-    ({'version': '3.2'}, False)
+    ({'version': '3.2'}, False),
+    ({'version': '3.4'}, False)
 ], ids=[
     'No information',
     'Valid current version',
