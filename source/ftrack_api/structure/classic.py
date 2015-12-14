10ed694400d21e9a50b0047006a2491cfbf71d9c
Mattias Lagergren <mattias.lagergren@ftrack.com>
2015-12-14 13:59:52 +0100

    Add support for sequence and container components

--- source/ftrack_api/structure/classic.py
+++ source/ftrack_api/structure/classic.py
@@ -6,8 +6,7 @@ import ftrack_api.structure.base
 
 class ClassicStructure(ftrack_api.structure.base.Structure):
 
-    def get_resource_identifier(self, entity, context=None):
-
+    def _get_fragments(self, entity):
         session = entity.session
         component = session.query(
             'Component where id is "{0}"'.format(
@@ -21,22 +20,54 @@ class ClassicStructure(ftrack_api.structure.base.Structure):
         ]
 
         project_id = component['version']['link'][0]['id']
-        project = session.query(
-            'Project where id is {}'.format(project_id)
-        ).one()
+        project = session.get('Project', project_id)
         asset = component['version']['asset']
 
         version_number = 'v{0:03d}'.format(component['version']['version'])
-
-        file_name = component['name'] + component['file_type']
 
         fragments = []
         fragments.append(project['name'])
         fragments.extend(structure_names)
         fragments.append(asset['name'])
         fragments.append(version_number)
-        fragments.append(file_name)
 
-        resource_identifier = self.path_separator.join(fragments)
+        return fragments
+
+    def get_resource_identifier(self, entity, context=None):
+        if entity.entity_type in ('FileComponent',):
+            container = entity['container']
+            if container and container is not ftrack_api.symbol.NOT_SET:
+                if container.entity_type in ('SequenceComponent',):
+                    name = '{0}.{1}{2}'.format(
+                        container['name'], entity['name'], entity['file_type']
+                    )
+
+                else:
+                    name = entity['name'] + entity['file_type']
+
+                fragments = self._get_fragments(container)
+
+            else:
+                name = entity['name'] + entity['file_type']
+                fragments = self._get_fragments(entity)
+
+            fragments.append(name)
+
+        elif entity.entity_type in ('SequenceComponent',):
+            fragments = self._get_fragments(entity)
+            sequence_expression = self._get_sequence_expression(entity)
+            fragments.append(
+                '{}.{}{}'.format(
+                    entity['name'], sequence_expression,
+                    entity['file_type']
+                )
+            )
+
+        elif entity.entity_type in ('ContainerComponent',):
+            fragments = self._get_fragments(entity)
+
+        resource_identifier = self.path_separator.join(
+            fragments
+        ).replace(' ', '_').lower()
 
         return resource_identifier
