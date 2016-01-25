2f9d6368285fffdf1e1b7a92c90dda41a15b2f6a
Mattias Lagergren <mattias.lagergren@ftrack.com>
2016-01-25 16:51:07 +0100

    Add more steps to location scenario

--- source/ftrack_api/_location_scenario.py
+++ source/ftrack_api/_location_scenario.py
@@ -1,8 +1,6 @@
 import logging
 import json
 
-logging.basicConfig(level=logging.DEBUG)
-
 
 class CentralizedLocationScenario(object):
 
@@ -16,9 +14,106 @@ class CentralizedLocationScenario(object):
         ).one()
 
     def configure_scenario(self, event):
+
+        steps = (
+            'select_scenario',
+            'select_location',
+            'configure_location',
+            'select_structure',
+            'select_mount_point',
+            'confirm_summary',
+            'save_configuration'
+        )
+
         values = event['data'].get('values', {})
 
-        if 'linux_mount_point' not in values and 'configuration' not in values:
+        # Calculate previous step and the next.
+        previous_step = values.get('step', 'select_scenario')
+        next_step = steps[steps.index(previous_step) + 1]
+        summary = False
+
+        print 'Previous step', previous_step, ' Next step: ', next_step
+
+        if 'configuration' in values:
+            configuration = values.pop('configuration')
+        else:
+            configuration = {}
+
+        if values:
+            # Update configuration with values from the previous step.
+            configuration[previous_step] = values
+
+        if next_step == 'select_location':
+            location_scenario = json.loads(self.location_scenario['value'])
+            try:
+                location_id = location_scenario['data']['location_id']
+            except (KeyError, TypeError):
+                location_id = None
+                pass
+
+            options = [{
+                'label': 'Create new location',
+                'value': 'create_new_location'
+            }]
+            for location in self.session.query(
+                'select name, label, description from Location'
+            ):
+                if location['name'] not in (
+                    'ftrack.origin', 'ftrack.unmanaged', 'ftrack.connect',
+                    'ftrack.server', 'ftrack.review'
+                ):
+                    options.append({
+                        'label': '{label} ({name})'.format(
+                            label=location['label'], name=location['name']
+                        ),
+                        'description': location['description'],
+                        'value': location['id']
+                    })
+
+            items = [{
+                'type': 'enumerator',
+                'label': 'Location',
+                'name': 'location_id',
+                'value': location_id,
+                'data': options
+            }]
+
+        if next_step == 'configure_location':
+
+            if values.get('location_id') == 'create_new_location':
+                # Add options to create a new location.
+                items = [{
+                    'label': 'Label',
+                    'name': 'location_label',
+                    'type': 'text'
+                }, {
+                    'label': 'Name',
+                    'name': 'location_name',
+                    'type': 'text'
+                }, {
+                    'label': 'Description',
+                    'name': 'location_description',
+                    'type': 'text'
+                }]
+
+            else:
+                # The user selected an existing location. Move on to next
+                # step.
+                next_step = 'select_structure'
+
+        if next_step == 'select_structure':
+            items = [{
+                'type': 'enumerator',
+                'label': 'Structure',
+                'name': 'structure_id',
+                'value': 'standard',
+                'data': [{
+                    'label': 'Standard',
+                    'value': 'standard'
+                }]
+            }]
+
+        if next_step == 'select_mount_point':
             description = (
                 'Set mount points for your centralized storage location.'
             )
@@ -31,11 +126,11 @@ class CentralizedLocationScenario(object):
                     mount_points = (
                         location_scenario['data']['accessor']['mount_points']
                     )
-                except (KeyError, TypeError) as error:
+                except (KeyError, TypeError):
                     pass
 
-            return {
-                'items': [{
+            items = [
+                {
                     'value': description,
                     'type': 'label'
                 }, {
@@ -53,51 +148,69 @@ class CentralizedLocationScenario(object):
                     'label': 'Windows',
                     'name': 'windows_mount_point',
                     'value': mount_points.get('windows', '')
-                }]
-            }
-        elif 'configuration' not in values:
-            summary = unicode(
-                'You will update the location scenario to use a Centralized '
-                'location. Your mount points are: \n\n'
-                '* Linux: {linux} \n'
-                '* OS X: {osx}\n'
-                '* Windows: {windows}\n\n'
-                'Notice that the location will not work properly on platforms '
-                'that doesn\'nt have a mount point filled in or accessible.'
-            ).format(
-                linux=values['linux_mount_point'],
-                osx=values['osx_mount_point'],
-                windows=values['windows_mount_point']
-            )
+                }
+            ]
 
-            return {
-                'summary': True,
-                'items': [{
-                    'type': 'label',
-                    'value': summary
-                }, {
-                    'type': 'hidden',
-                    'value': values,
-                    'name': 'configuration'
-                }]
-            }
-        else:
-            configuration = values['configuration']
+        if next_step == 'confirm_summary':
+            items = [{
+                'type': 'label',
+                'value': unicode(
+                    'You will update the location scenario to use a Centralized '
+                    'location. Your mount points are: \n\n'
+                    '* Linux: {linux} \n'
+                    '* OS X: {osx}\n'
+                    '* Windows: {windows}\n\n'
+                    'Notice that the location will not work properly on platforms '
+                    'that doesn\'nt have a mount point filled in or accessible.'
+                ).format(
+                    linux=values['linux_mount_point'],
+                    osx=values['osx_mount_point'],
+                    windows=values['windows_mount_point']
+                )
+            }]
+            summary = True
+
+        if next_step == 'save_configuration':
+            mount_points = configuration['select_mount_point']
+            select_location = configuration['select_location']
+
+            location = self.session.query(
+                'Location where id is "{0}"'.format(
+                    select_location['location_id']
+                )
+            ).one()
+
             self.location_scenario['value'] = json.dumps({
                 'scenario': self.scenario_name,
                 'data': {
-                    'location_id': '',
-                    'location_name': '',
+                    'location_id': select_location['location_id'],
+                    'location_name': location['name'],
                     'accessor': {
                         'mount_points': {
-                            'linux': configuration['linux_mount_point'],
-                            'osx': configuration['osx_mount_point'],
-                            'windows': configuration['windows_mount_point']
+                            'linux': mount_points['linux_mount_point'],
+                            'osx': mount_points['osx_mount_point'],
+                            'windows': mount_points['windows_mount_point']
                         }
                     }
                 }
             })
             self.session.commit()
+
+        items.append({
+            'type': 'hidden',
+            'value': configuration,
+            'name': 'configuration'
+        })
+        items.append({
+            'type': 'hidden',
+            'value': next_step,
+            'name': 'step'
+        })
+
+        return {
+            'items': items,
+            'summary': summary
+        }
 
     def discover_centralized_scenario(self, event):
         return {
