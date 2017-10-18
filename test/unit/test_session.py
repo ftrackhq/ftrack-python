c0122ea7b9c73aba282d86501dac109bb3aa4683
Eric Hermelin <erich@cinesite.com>
2017-10-18 14:28:33 +0200

    separate the unit test, and skip the ldap sync by default

--- test/unit/test_session.py
+++ test/unit/test_session.py
@@ -1183,10 +1183,19 @@ def test_context_manager(session):
 
     assert session.closed is True
 
-@pytest.mark.slow
+
 def test_delayed_job(session):
     '''Test the delayed_job action'''
 
+    with pytest.raises(ValueError):
+        session.delayed_job(
+            'DUMMY_JOB'
+        )
+
+
+@pytest.mark.skip(reason='No configured ldap server.')
+def test_delayed_job_ldap_sync(session):
+    '''Test the a delayed_job ldap sync action'''
     result = session.delayed_job(
         ftrack_api.symbol.JOB_SYNC_USERS_LDAP
     )
@@ -1194,9 +1203,3 @@ def test_delayed_job(session):
     assert isinstance(
         result, ftrack_api.entity.job.Job
     )
-
-    with pytest.raises(ValueError):
-        session.delayed_job(
-            'DUMMY_JOB'
-        )
-
