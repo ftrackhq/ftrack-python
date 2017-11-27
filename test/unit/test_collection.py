a74db90c8459405d10c539c31482c184707ae3f4
Björn Rydahl <bjorn.rydahl@gmail.com>
2017-11-27 14:45:28 -0800

    Disable event hub in test to avoid logger output.

--- test/unit/test_collection.py
+++ test/unit/test_collection.py
@@ -483,9 +483,9 @@ def test_mapped_collection_on_create(session, unique_name, project):
         assert value == task['metadata'][key]
 
 
-def test_collection_refresh(new_asset_version, new_component):
+def test_collection_refresh(new_asset_version, new_component, session):
     '''Test collection reload.'''
-    session_two = ftrack_api.Session()
+    session_two = ftrack_api.Session(auto_connect_event_hub=False)
 
     query_string = 'select components from AssetVersion where id is "{0}"'.format(
         new_asset_version.get('id')
@@ -528,7 +528,7 @@ def test_collection_refresh(new_asset_version, new_component):
 
 def test_mapped_collection_reload(new_asset_version):
     '''Test mapped collection reload.'''
-    session_two = ftrack_api.Session()
+    session_two = ftrack_api.Session(auto_connect_event_hub=False)
 
     query_string = 'select metadata from AssetVersion where id is "{0}"'.format(
         new_asset_version.get('id')
@@ -572,6 +572,3 @@ def test_mapped_collection_reload(new_asset_version):
     assert (
         new_asset_version_two['metadata']['test'] == local_data
     )
-
-
-
