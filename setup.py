a45fc725ca2d78f20d250c8aa5deae1753fb0dd6
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2018-04-06 15:43:47 +0200

    fix version of sphinx < 1.6 to avoid testing issues

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         '': 'source'
     },
     setup_requires=[
-        'sphinx >= 1.2.2, < 2',
+        'sphinx >= 1.2.2, < 1.6',
         'sphinx_rtd_theme >= 0.1.6, < 1',
         'lowdown >= 0.1.0, < 2'
     ],
