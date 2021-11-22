d9db1968fa93f033b168c467f42b1808c5611098
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2021-11-22 16:52:00 +0100

    fix docstring

--- source/ftrack_api/structure/entity_id.py
+++ source/ftrack_api/structure/entity_id.py
@@ -8,5 +8,5 @@ class EntityIdStructure(ftrack_api.structure.base.Structure):
     '''Entity id pass-through structure.'''
 
     def get_resource_identifier(self, entity, context=None):
-        '''Return a *resourceIdentifier* for supplied *entity*.'''
+        '''Return a *entity Id* for supplied *entity*.'''
         return entity['id']
