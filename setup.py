16c3f3edb81bf8131e4c370dc4867dc75cb246c1
lorenzo <lorenzo.angeli@ftrack.com>
2021-11-26 11:12:02 +0100

    review PIPY classifiers and enforce more strict python requires, to ensure we target the right python versions.

--- setup.py
+++ setup.py
@@ -100,9 +100,13 @@ setup(
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+
     ],
     zip_safe=False,
-    python_requires=">=2.7.9, <4.0"
+    python_requires=">=2.7.9, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, < 3.10"
 )
