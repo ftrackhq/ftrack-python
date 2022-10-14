9b0316fe6ba28aa26e96596052ba3ba8cbb4558d
lorenzo angeli <hdd@users.noreply.github.com>
2022-10-14 10:37:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
         'sphinx >= 1.2.2, < 1.6',
         'sphinx_rtd_theme >= 0.1.6, < 1',
         'lowdown >= 0.1.0, < 2',
-        'setuptools_scm >=5, <8',
+        'setuptools_scm',
         'wheel'
     ],
     install_requires=[
