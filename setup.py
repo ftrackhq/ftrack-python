9e5659dceae0782fabfad346b8a73077415db68d
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2022-10-19 11:42:54 +0200

    lock certify for python 2.7

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(
         'setuptools_scm >=6; python_version > "3.6"',
         'setuptools < 45; python_version < "3.0"',
         'requests < 2.28.0; python_version <= "3.0"',
+        'certifi <= 2021.10.8; python_version <= "3.0"'
     ],
     install_requires=[
         'typing-extensions <= 4.1.1 ; python_version <= "3.6"',
