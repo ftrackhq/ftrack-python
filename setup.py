b1a4a0992a1d8045211808d14425310ef2c84979
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2022-10-19 12:08:51 +0200

    fix typo in test_requires

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
         'pytest >= 4.6, <7; python_version <= "3.6"',
         'pytest-mock',
         'mock <= 3.0.5; python_version <= "3.0"',
-        'mock > 3.0.5  python_version > "3.0"',
+        'mock > 3.0.5;  python_version > "3.0"',
         'flaky'
     ],
     cmdclass={
