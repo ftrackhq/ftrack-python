cff1d32b62f84e5c0142266cfe1d9de7bca98ac2
Eric Hermelin <eric.hermelin@ftrack.com>
2023-10-26 16:10:42 +0200

    fix: incomplete documentation builds (#24)

--- doc/conf.py
+++ doc/conf.py
@@ -69,7 +69,7 @@ html_copy_source = True
 
 # -- Autodoc ------------------------------------------------------------------
 
-autodoc_default_flags = ['members', 'undoc-members', 'inherited-members']
+autodoc_default_options = {"members": None, 'undoc-members': None, 'inherited-members': None}
 autodoc_member_order = 'bysource'
 
 
