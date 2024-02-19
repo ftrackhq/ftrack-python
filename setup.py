9081a4e20696df5abf2d27e62aae18895d0d67ce
Octavian Ionescu <octavian.ionescu@backlight.co>
2024-02-19 13:44:55 +0200

    dropped 2.7, 3.6, 3.7

--- setup.py
+++ setup.py
@@ -98,9 +98,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
@@ -108,5 +105,5 @@ setup(
 
     ],
     zip_safe=False,
-    python_requires=">=2.7.9, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*"
+    python_requires=">=3.8.*"
 )
