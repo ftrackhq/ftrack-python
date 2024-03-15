d36d2f5c5c4ccba3fb2d6ef63f8fe5956d72bba7
erhe <eric.hermelin@ftrack.com>
2024-03-15 16:04:31 +0100

    remove python 2 version.

--- source/ftrack_api/plugin.py
+++ source/ftrack_api/plugin.py
@@ -9,64 +9,56 @@ import os
 import uuid
 import traceback
 
-try:
-    from inspect import getfullargspec
+import importlib.util
+import importlib.machinery
 
-except ImportError:
-    # getargspec is deprecated in version 3.0. convert `ArgSpec` to a named
-    # tuple `FullArgSpec`. We only rely on the values of varargs and varkw.
 
-    # Implemented with "https://github.com/tensorflow/tensorflow/blob/3d69fd003d4acef0ea5663a4794c1e9a4f6ec998/tensorflow/python/util/tf_inspect.py#L34"
-    # as reference.
-    import inspect
+import inspect
 
-    FullArgSpec = collections.namedtuple(
-        "FullArgSpec",
-        [
-            "args",
-            "varargs",
-            "varkw",
-            "defaults",
-            "kwonlyargs",
-            "kwonlydefaults",
-            "annotations",
-        ],
+FullArgSpec = collections.namedtuple(
+    "FullArgSpec",
+    [
+        "args",
+        "varargs",
+        "varkw",
+        "defaults",
+        "kwonlyargs",
+        "kwonlydefaults",
+        "annotations",
+    ],
+)
+
+
+def getfullargspec(func):
+    """a python 2 version of `getfullargspec`. getargspec is deprecated in version 3.0. convert `ArgSpec`
+    to a named tuple `FullArgSpec`. We only rely on the values of varargs and varkw.
+
+    Implemented with "https://github.com/tensorflow/tensorflow/blob/3d69fd003d4acef0ea5663a4794c1e9a4f6ec998/tensorflow/python/util/tf_inspect.py#L34"
+    as reference."""
+    spec = inspect.getargspec(func)
+
+    return FullArgSpec(
+        args=spec.args,
+        varargs=spec.varargs,
+        varkw=spec.keywords,
+        defaults=spec.defaults,
+        kwonlyargs=[],
+        kwonlydefaults=None,
+        annotations={},
     )
 
-    def getfullargspec(func):
-        """a python 2 version of `getfullargspec`."""
-        spec = inspect.getargspec(func)
 
-        return FullArgSpec(
-            args=spec.args,
-            varargs=spec.varargs,
-            varkw=spec.keywords,
-            defaults=spec.defaults,
-            kwonlyargs=[],
-            kwonlydefaults=None,
-            annotations={},
-        )
+def load_source(modname, filename):
+    # https://docs.python.org/3/whatsnew/3.12.html#imp
 
+    loader = importlib.machinery.SourceFileLoader(modname, filename)
+    module = importlib.util.module_from_spec(
+        importlib.util.spec_from_file_location(modname, filename, loader=loader)
+    )
 
-try:
-    from imp import load_source
+    loader.exec_module(module)
 
-except ImportError:
-    # The imp module is deprecated in version 3.12. Use importlib instead.
-    import importlib.util
-    import importlib.machinery
-
-    def load_source(modname, filename):
-        # https://docs.python.org/3/whatsnew/3.12.html#imp
-
-        loader = importlib.machinery.SourceFileLoader(modname, filename)
-        module = importlib.util.module_from_spec(
-            importlib.util.spec_from_file_location(modname, filename, loader=loader)
-        )
-
-        loader.exec_module(module)
-
-        return module
+    return module
 
 
 def discover(paths, positional_arguments=None, keyword_arguments=None):
